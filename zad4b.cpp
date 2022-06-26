fbaaac9f6fd243691723a6211de5744524d73674
NN <n.p.nikolov01@gmail.com>
2022-06-26 19:19:36 +0300

    Pi approximation using Leibniz series

---
+++ zad4b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+
+long double leibnizSeries();
+
+int main()
+{
+	std::cout.precision(16);
+	
+	leibnizSeries();
+	return 0;
+}
+
+long double leibnizSeries()
+{
+	long double sum=0;
+	int sign = 1;
+	long double tmp=0;
+	int cnt=1;
+	
+	long double diff = 0;
+	do{
+		tmp = sum;
+		sum += sign*1.0/(2*cnt-1);
+		sign = -sign;
+		cnt++;
+	}while( fabs(sum-tmp) > 0.000000001 );
+	
+	std::cout << "Approximation after " << cnt-1 << " loops:" << '\n' << 4*sum << '\n';
+	
+	return 4*sum;
+}
